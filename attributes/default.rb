0a33bffef36d239bc78804aac2e2dad5b5b5ef38
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-01-01 17:58:33 +0100

    add attributes

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['xbmc']['prefix']   = "/usr/local"
+default['xbmc']['location']   = "/opt"
+default['xbmc']['user']   = "xbmc"
+
+default['xbmc']['default_configure_flags'] = Array.new
+
+default['xbmc']['force_recompile']  = false
